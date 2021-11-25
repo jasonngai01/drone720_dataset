f7c416a33d978cc6585b8c6d595560e5f80192b9
unknown <jason.k.ngai@gmail.com>
2021-11-25 09:01:44 +0800

    'st'

---
+++ code/uav/README.md
@@ -0,0 +1,7 @@
+TensorFlow Lite UAV Audio Recognition 
+
+1. Terminal 1 >> sudo ntpdate 192.168.1.211 <command post pc>
+
+2. Terminal 2 >> cd uav >> python3 web_server.py <activate the web server for publishing the result message>
+
+3. Terminal 3 >> python3 uav.py <activate the uav detection code to detect any drone and the environment sound>
---
+++ code/uav/install-deps.sh
@@ -0,0 +1,18 @@
+# using the Qengineering DNN image
+
+sudo apt update
+sudo apt install -y libhdf5-dev
+sudo apt install -y libatlas-base-dev
+sudo apt install -y screen
+
+sudo apt-get update 
+sudo apt-get upgrade 
+sudo apt-get dist-upgrade
+sudo apt-get install portaudio19-dev 
+sudo pip install pyaudio
+
+
+pip3 install soundfile
+
+pip3 install pandas
+
